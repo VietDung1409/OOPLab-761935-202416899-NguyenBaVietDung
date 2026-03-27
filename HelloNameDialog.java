0c7aa86ab78b6cb41d2046d4f083659c8cada2ae
NguyenBaVietDung <bdnguyen100@gmail.com>
2026-03-27 13:44:34 +0700

    da hoan thanh 2.2.4

--- HelloNameDialog.java
+++ HelloNameDialog.java
@@ -3,7 +3,7 @@ public class HelloNameDialog {
     public static void main(String [] args){
         String result;
         result = JOptionPane.showInputDialog("Please enter your name: ");
-        JOptionPane.showMessageDialog(null,"Hi" + result + "!");
+        JOptionPane.showMessageDialog(null,"Hi " + result + "!");
         System.exit(0);
     }
 }
