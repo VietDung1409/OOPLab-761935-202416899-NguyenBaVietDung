129df1f5be2af388fd54e1d68cb2d6d13b902915
Nguyen Ba Viet Dung <bdnguyen100@gmail.com>
2026-04-17 13:40:58 +0700

    section5

---
+++ hust/soict/dsai/aims/store/DigitalVideoDisc.java
@@ -0,0 +1,27 @@
+package hust.soict.dsai.aims.store;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private double cost;
+
+    public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "DVD - " + title + " - " + category + " - " 
+             + director + " - " + length + ": " + cost + " $";
+    }
+}
